02de208387ac76ac740c5420d12e94b1dcaa9776
win-ts <winthanatpong@gmail.com>
2024-09-23 11:36:10 +0700

    chore: set timeout to 30 sec

--- pkg/httpclient/_example/main.go
+++ pkg/httpclient/_example/main.go
@@ -2,19 +2,21 @@
 package main
 
 import (
-	"log"
+	"time"
+
+	"github.com/labstack/gommon/log"
 
 	"github.com/ShiftOver/shiftover-backend/pkg/httpclient"
 )
 
 func main() {
-	c := httpclient.NewClient(httpclient.SetMaxRetry(3), httpclient.SetTimeout(10))
+	c := httpclient.NewClient(httpclient.SetMaxRetry(3), httpclient.SetTimeout(30 * time.Second))
 
-	response, err := c.Get("http://example.com", httpclient.SendOptions{
-		"queries": map[string]string{
-			"queries": "example",
-			"limit":   "10",
-		},
+	response, err := c.Get("https://66f0ebddf2a8bce81be6fcb0.mockapi.io/api/v1/test", httpclient.SendOptions{
+		// "queries": map[string]string{
+		// 	"queries": "example",
+		// 	"limit":   "10",
+		// },
 		"headers": map[string]string{
 			"custom":       "example",
 			"Content-Type": "application/json",
@@ -23,5 +25,8 @@ func main() {
 	if err != nil {
 		log.Panicf("error - [main]: unable to get response: %v", err)
 	}
-	_ = response
+
+	log.Infof("Status: %v\n", response.StatusCode)
+	log.Infof("Headers: %v\n", response.Header)
+	log.Infof("Body: %v\n", string(response.Body))
 }
