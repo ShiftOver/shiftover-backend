ff56aa217cbdd0e4fad2a49bfb16f34af7aab2e3
Thanatpong [Win] Songjitsomboon <winthanatpong@gmail.com>
2025-02-18 10:19:02 +0700

    feat: upsert patient assessment

--- handler/patient_assessment_handler.go
+++ handler/patient_assessment_handler.go
@@ -2,8 +2,11 @@ package handler
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/ShiftOver/shiftover-backend/dto"
+	"github.com/ShiftOver/shiftover-backend/pkg/request"
 	"github.com/ShiftOver/shiftover-backend/pkg/response"
 	"github.com/labstack/echo/v4"
 	"github.com/pkg/errors"
@@ -26,4 +29,30 @@ func (h *httpHandler) GetPatientAssessment(c echo.Context) error {
 	}
 
 	return response.SuccessResponse(c, http.StatusOK, patientAssessment)
+}
+
+// UpsertPatientAssessment is a handler function to upsert a patient assessment
+// @Summary Upsert a patientAssessment
+// @Description Upsert a patientAssessment into the database
+// @Tags PatientAssessment
+// @Accept json
+// @Produce json
+// @Param patientAssessment body dto.PatientAssessmentEntity true "PatientAssessment"
+// @Success 200 {string} string "PatientAssessment upserted successfully"
+// @Router /v1/patient/assessment [post]
+func (h *httpHandler) UpsertPatientAssessment(c echo.Context) error {
+	ctx := context.Background()
+	wrapper := request.ContextWrapper(c)
+
+	var payload dto.PatientAssessmentEntity
+	if err := wrapper.Bind(&payload); err != nil {
+		return response.ErrResponse(c, http.StatusBadRequest, fmt.Sprintf("error - [UpsertPatientAssessment]: unable to bind payload: %v", err))
+	}
+
+	err := h.d.Service.UpsertPatientAssessment(ctx, payload)
+	if err != nil {
+		return response.ErrResponse(c, http.StatusInternalServerError, errors.Wrap(err, "error - [UpsertPatientAssessment]: unable to upsert patientAssessment").Error())
+	}
+
+	return response.SuccessResponse(c, http.StatusOK, "PatientAssessment upserted successfully")
 }
